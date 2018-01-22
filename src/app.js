ff41c6aaca444c59c7707fd7f7f176e2769a731c
beeth0ven <beeth0vendev@163.com>
2018-01-22 17:30:21 +0800

    Setup babel and webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  (<h1>It's ok</h1>),
+  document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/app.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js"
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "env",
+              "stage-0",
+              "react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
