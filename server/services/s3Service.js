a739425460c1a26e74f0b9f7740c70299f611e46
beeth0ven <beeth0vendev@gmail.com>
2018-03-09 23:06:00 +0800

    Valid cros configuration

--- server/services/s3Service.js
+++ server/services/s3Service.js
@@ -6,23 +6,31 @@ const s3 = new S3({
   accessKeyId: S3_ACCESS_KEY_ID,
   secretAccessKey: S3_SECRET_ACCESS_KEY,
   region: 'home',
-  endpoint: 'http://127.0.0.1:9000' ,
+  endpoint: 'http://home.beeth0ven.cf:9000' ,
   s3ForcePathStyle: true, // needed with minio?
   signatureVersion: 'v4',
-  headers: {'Access-Control-Allow-Origin': '*'},
 });
+
 
 const getS3 = () => s3;
 
 const s3Service = new S3Router({
+  headers: {'Access-Control-Allow-Origin': '*'},
   bucket: S3_BUCKET,
   ACL: 'public-read',
   getS3,
 });
 
+
 (async () => {
-  const log = await s3.listBuckets().promise();
-  console.log(log);
+
+  try {
+    const log = await s3.listBuckets().promise();
+    console.log(log);
+  } catch (error) {
+    console.error(error)
+  }
+
 })();
 
 export { s3Service };
